a44db1811971e3c39220d9deec29d8fb65e2c5a0
vanessamj99 <vmj232@nyu.edu>
2022-11-05 14:05:19 -0700

    solidity file

---
+++ solidity/LoveSol.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.4.23;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.4/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.4/contracts/utils/Counters.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.4/contracts/access/Ownable.sol";
+
+contract LoveBox is ERC721{
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    constructor(){
+        ERC721(_name,_symbol);
+    }
+    function mintToken(address _to, string memory tokenURI) returns (uint256){
+        unint newTokenId = _tokenIds.current();
+        mint(_to,newTokenId);
+        _setTokenUri(newTokenId,tokenURI);
+        _tokenIds.increment();
+        return newTokenId;
+    }
+    function send(address _to, string memory tokenURI){
+        mintToken(_to, tokenURI);
+    }
+}
