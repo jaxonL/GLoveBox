645e9e476ae232c0d983d2e6496547fd8af68cbf
vanessamj99 <vmj232@nyu.edu>
2022-11-05 14:30:45 -0700

    file edits

--- solidity/GloveSol.sol
+++ solidity/GloveSol.sol
@@ -1,10 +1,10 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.4.23;
+pragma solidity ^0.8.7;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.4/contracts/token/ERC721/ERC721.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.4/contracts/utils/Counters.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.4/contracts/access/Ownable.sol";
 
-contract LoveBox is ERC721{
+contract GloveBox is ERC721{
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     constructor(){
