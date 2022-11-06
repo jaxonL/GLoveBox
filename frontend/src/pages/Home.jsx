1d258c474adfedfbb4fbd0a1bb887145779abd88
Jaxon Lin <11150920+jaxonL@users.noreply.github.com>
2022-11-05 17:52:22 -0700

    add material; use controlled textfield

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,19 @@
+import React, { useState } from 'react';
 import { Nav } from '../components/nav/Nav';
+import { Button, TextField } from '@mui/material';
 import './Home.css';
 
 export function Home() {
+  const [message, setMessage] = useState('');
+
+  const onMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
+
   const sendButtonClicked = (e) => {
     e.preventDefault();
-    console.log('send button clicked');
+    console.log('send button clicked', message);
+    // get connected address
   };
 
   return (
@@ -15,10 +24,17 @@ export function Home() {
           send and receive messages to strangers that live forever
         </div>
         <p>write a nice message to a stranger:</p>
-        <textarea id="messageInput"></textarea>
-        <button id="sendButton" type="submit" onClick={sendButtonClicked}>
+        <TextField
+          id="messageInput"
+          multiline
+          fullWidth
+          rows={10}
+          value={message}
+          onChange={onMessageChange}
+        />
+        <Button id="sendButton" type="submit" onClick={sendButtonClicked}>
           send &lt;3
-        </button>
+        </Button>
       </div>
     </>
   );
