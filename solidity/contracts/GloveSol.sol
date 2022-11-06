6944d44748cbe847eb555eb5e97cdba9bf80cb42
Jaxon Lin <11150920+jaxonL@users.noreply.github.com>
2022-11-06 01:59:47 -0800

    update with message data get fn

--- solidity/contracts/GloveSol.sol
+++ solidity/contracts/GloveSol.sol
@@ -3,26 +3,73 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract GloveBox is ERC721{
+contract GloveBox is ERC721, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    // Optional mapping for token URIs
-    mapping (uint256 => string) private _tokenURIs;
-    constructor() ERC721("Love Message","GLB") {}
+    address[] participants;
+    mapping(address=>TokenMetaData[]) public ownershipRecord;
+    // mapping for token -> metadata
+    mapping(uint=>string) public tokenIdToMessage;
 
-    function mintToken(address _to, string memory tokenURI) public returns (uint256){
+    struct TokenMetaData {
+        uint tokenId;
+        uint timeStamp;
+        string tokenURI;
+    }
+
+    constructor() ERC721("GLoveBoxMessage","GLB") {}
+
+    modifier callerIsUser() {
+        require(tx.origin == msg.sender, "GLoveBoxMessage :: Cannot be called by a contract");
+        _;
+    }
+
+    function sendMessage(string memory message) public returns (uint256) {
         uint newTokenId = _tokenIds.current();
-        _mint(_to,newTokenId);
-        _setTokenURI(newTokenId,tokenURI);
+        uint random = uint(keccak256(abi.encodePacked(block.timestamp, message, msg.sender))) % participants.length;
+        _safeMint(participants[random], newTokenId);
+        ownershipRecord[participants[random]].push(TokenMetaData(newTokenId, block.timestamp, message));
+        tokenIdToMessage[newTokenId] = message;
         _tokenIds.increment();
         return newTokenId;
     }
-    function send(address _to, string memory tokenURI) public{
-        mintToken(_to, tokenURI);
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        return tokenIdToMessage[tokenId];
     }
-      function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
-        require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
-        _tokenURIs[tokenId] = _tokenURI;
+
+    // used with balanceOf to rebuild the received messages
+    function getMessageData(uint index) public view returns (uint, uint, string memory) {
+        require(index < ownershipRecord[msg.sender].length, "GetMessageData: query for nonexistent index");
+        TokenMetaData storage metadata = ownershipRecord[msg.sender][index];
+        return (metadata.tokenId, metadata.timeStamp, metadata.tokenURI);
+    }
+
+    // registration
+    function register() external callerIsUser {
+        // check if this address already registered
+        for (uint i; i < participants.length; i++) {
+            if (participants[i] == msg.sender) {
+                revert("Already registered");
+            }
+        }
+        participants.push(msg.sender);
+    }
+
+    function getParticipants () public view returns (address[] memory) {
+        return participants;
+    }
+
+    function isRegistered (address participant) public view returns (bool) {
+        // check if this address already registered
+        for (uint i; i < participants.length; i++) {
+            if (participants[i] == participant) {
+                return true;
+            }
+        }
+        return false;
     }
 }
