215445ab6c6fd43271ffc7db4c97e44badc7aaf9
Jaxon Lin <11150920+jaxonL@users.noreply.github.com>
2022-11-05 21:23:48 -0700

    add constants

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,4 @@
-const kGloveBoxAddress = '';
+// export const kGloveBoxAddress = '0x966314C1D418bf65D7647B52f29A5b2a684644E1';
+export const kGloveBoxAddress = '0xf1cC257BB824511D108Bd671744EAFf960353BfB';
 export const kSkaleChainId = 647_426_021;
+export const kSkaleEndpoint = 'https://eth-sf.skalenodes.com/v1/hackathon-complex-easy-naos';
