537fca128bb60790093724078a04ddac5f56f5ef
Jaxon Lin <11150920+jaxonL@users.noreply.github.com>
2022-11-16 22:33:01 -0500

    rename contract; return list of received messages

--- solidity/contracts/GLoveBox.sol
+++ solidity/contracts/GLoveBox.sol
@@ -5,32 +5,34 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract GloveBox is ERC721, Ownable {
+contract GLoveBox is ERC721, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     address[] participants;
     mapping(address=>TokenMetaData[]) public ownershipRecord;
-    // mapping for token -> metadata
+    // v1: mapping for token -> metadata
     mapping(uint=>string) public tokenIdToMessage;
 
     struct TokenMetaData {
         uint tokenId;
         uint timeStamp;
         string tokenURI;
+        address from;
     }
 
-    constructor() ERC721("GLoveBoxMessage","GLB") {}
+    constructor() ERC721("GLoveBox","GLB") {}
 
     modifier callerIsUser() {
-        require(tx.origin == msg.sender, "GLoveBoxMessage :: Cannot be called by a contract");
+        require(tx.origin == msg.sender, "GLoveBox :: Cannot be called by a contract");
         _;
     }
 
     function sendMessage(string memory message) public returns (uint256) {
         uint newTokenId = _tokenIds.current();
+        require(participants.length > 0, "GloveBox :: Empty participant list");
         uint random = uint(keccak256(abi.encodePacked(block.timestamp, message, msg.sender))) % participants.length;
         _safeMint(participants[random], newTokenId);
-        ownershipRecord[participants[random]].push(TokenMetaData(newTokenId, block.timestamp, message));
+        ownershipRecord[participants[random]].push(TokenMetaData(newTokenId, block.timestamp, message, msg.sender));
         tokenIdToMessage[newTokenId] = message;
         _tokenIds.increment();
         return newTokenId;
@@ -42,10 +44,9 @@ contract GloveBox is ERC721, Ownable {
     }
 
     // used with balanceOf to rebuild the received messages
-    function getMessageData(uint index) public view returns (uint, uint, string memory) {
-        require(index < ownershipRecord[msg.sender].length, "GetMessageData: query for nonexistent index");
-        TokenMetaData storage metadata = ownershipRecord[msg.sender][index];
-        return (metadata.tokenId, metadata.timeStamp, metadata.tokenURI);
+    function getReceivedMessages() public view returns (TokenMetaData[] memory receivedMessages) {
+        receivedMessages = ownershipRecord[msg.sender];
+        return receivedMessages;
     }
 
     // registration
