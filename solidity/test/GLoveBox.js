488b23f62ce1cef9496cb2480f84c443c2d13511
Jaxon Lin <11150920+jaxonL@users.noreply.github.com>
2022-12-03 15:40:08 -0500

    test: add test fixture

--- solidity/test/GLoveBox.js
+++ solidity/test/GLoveBox.js
@@ -1,13 +1,18 @@
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { expect } = require('chai');
 
 describe('GLoveBox contract', function () {
-  it('should populate the participants list with the owner', async function () {
-    const [owner] = await ethers.getSigners();
+  async function deployFixture() {
+    const [owner, addr1, addr2] = await ethers.getSigners();
     const GLoveBox = await ethers.getContractFactory('GLoveBox');
+    const glb = await GLoveBox.deploy();
+    await glb.deployed();
+    return { GLoveBox, glb, owner, addr1, addr2 };
+  }
+  it('should populate the participants list with the owner', async function () {
+    const { owner, glb } = await loadFixture(deployFixture);
 
-    const hardhatGlb = await GLoveBox.deploy();
-
-    const isOwnerRegistered = await hardhatGlb.isRegistered(owner.address);
+    const isOwnerRegistered = await glb.isRegistered(owner.address);
     expect(isOwnerRegistered).to.be.true;
   });
 });
