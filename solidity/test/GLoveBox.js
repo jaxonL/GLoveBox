9df6b63ebb3add307e1c4b481a3b31236bf4830f
Jaxon Lin <11150920+jaxonL@users.noreply.github.com>
2022-12-03 15:59:21 -0500

    test: add tests on registration

--- solidity/test/GLoveBox.js
+++ solidity/test/GLoveBox.js
@@ -1,5 +1,10 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+const {
+  loadFixture,
+  impersonateAccount,
+  setBalance,
+} = require('@nomicfoundation/hardhat-network-helpers');
 const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
 describe('GLoveBox contract', function () {
   async function deployFixture() {
@@ -15,4 +20,46 @@ describe('GLoveBox contract', function () {
     const isOwnerRegistered = await glb.isRegistered(owner.address);
     expect(isOwnerRegistered).to.be.true;
   });
+
+  it('should register a new participant', async function () {
+    const { addr1, glb } = await loadFixture(deployFixture);
+
+    await glb.connect(addr1).register();
+    const isAddr1Registered = await glb.isRegistered(addr1.address);
+    expect(isAddr1Registered).to.be.true;
+  });
+
+  it('should revert when owner tries to register', async function () {
+    const { owner, glb } = await loadFixture(deployFixture);
+
+    await expect(glb.connect(owner).register()).to.be.revertedWith(
+      'GLoveBox :: Owner is already registered',
+    );
+  });
+
+  it('should not register a participant twice', async function () {
+    const { addr1, glb } = await loadFixture(deployFixture);
+
+    await glb.connect(addr1).register();
+    await expect(glb.connect(addr1).register()).to.be.revertedWith(
+      'GLoveBox :: Already registered',
+    );
+  });
+
+  // it('should reject registration from a contract', async function () {
+  //   const { GLoveBox, glb } = await loadFixture(deployFixture);
+
+  //   const glb2 = await GLoveBox.deploy();
+  //   await glb2.deployed();
+  //   await impersonateAccount(glb2.address);
+  //   await setBalance(glb2.address, 100n ** 18n);
+  //   const contractAddress = await ethers.getSigner(glb2.address);
+
+  //   await glb.connect(contractAddress).register();
+
+  //   expect(await glb.isRegistered(contractAddress.address)).to.be.false;
+  // });
+
+  // TODO: test send message
+  // TODO: test get messages
 });
