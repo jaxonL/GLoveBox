ce3c9a3ea8420678036e60c681ad09175000460b
Jaxon Lin <11150920+jaxonL@users.noreply.github.com>
2023-01-08 20:59:10 -0500

    feat: display messages

--- frontend/src/pages/MyGlovebox.jsx
+++ frontend/src/pages/MyGlovebox.jsx
@@ -6,6 +6,7 @@ import { kGloveBoxAddress, Network } from '../utils/constants';
 import { CircularProgress } from '@mui/material';
 import { Message } from '../components/message/Message';
 import { useNavigate } from 'react-router-dom';
+import { Message as MessageObject } from '../api/Message';
 
 const gloveboxContract = {
   address: kGloveBoxAddress[Network.GOERLI],
@@ -21,23 +22,34 @@ export function MyGlovebox() {
     },
   });
 
-  const [messageIndices, setMessageIndices] = useState([]);
+  const [messages, setMessages] = useState([]);
+  const [messageCount, setMessageCount] = useState(0);
 
   const { data: balanceOf, isLoading: loadingBalanceOf } = useContractRead({
     ...gloveboxContract,
     functionName: 'balanceOf',
     args: [address],
     onSettled(data, error) {
-      console.log('balance:', data.toNumber());
-      if (data.toNumber()) {
-        const indices = [];
-        for (let i = 0; i < data.toNumber(); i++) {
-          indices.push(i);
-        }
-        setMessageIndices(indices);
-      }
+      setMessageCount(data.toNumber());
     },
   });
+
+  const { data: receivedMessages, isLoading: loadingReceivedMessages } =
+    useContractRead({
+      ...gloveboxContract,
+      functionName: 'getReceivedMessages',
+      onSettled(data, error) {
+        const messages = [];
+        for (let i = 0; i < data.length; i++) {
+          const message = new MessageObject(...data[i]);
+          messages.push(message);
+        }
+        setMessages(messages);
+      },
+      overrides: {
+        from: address,
+      },
+    });
 
   return (
     <>
@@ -49,11 +61,16 @@ export function MyGlovebox() {
         <div className="messageGrid">
           {loadingBalanceOf ? (
             <CircularProgress />
-          ) : messageIndices.length ? (
-            messageIndices.map((index) => <Message index={index} key={index} />)
+          ) : messages.length ? (
+            messages.map((message) => (
+              <Message data={message} key={message.tokenId} />
+            ))
           ) : (
             'No messages'
           )}
+        </div>
+        <div className="messagecount">
+          you have a total of {messageCount} message{messageCount !== 1 && 's'}
         </div>
       </div>
     </>
